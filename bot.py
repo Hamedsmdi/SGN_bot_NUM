69c6a43fc4a1bba93bfc26fb08e1a0244cf550b1
Hamed Samadi <157298526+Hamedsmdi@users.noreply.github.com>
2025-01-26 12:25:03 +0330

    bot.py

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 import os
 
-BOT_TOKEN = os.getenv("7256899482:AAFtFnfKfpnGLA3qjBGlKMyVAsVMZ1-AXhw")
+BOT_TOKEN = "7256899482:AAFtFnfKfpnGLA3qjBGlKMyVAsVMZ1-AXhw"
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("سلام! به ربات ما خوش آمدید.")
